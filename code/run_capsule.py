af3fef1f5a550a5db8e3000fcee25b68a5c07766
Alessio Buccino <alejoe9187@gmail.com>
2024-12-16 17:13:49 +0100

    Capsule name in log

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
             session_name = data_description["name"]
 
         log.setup_logging(
-            "Job Dispatch Ecephys",
+            "Visualize Ecephys",
             mouse_id=subject_id,
             session_name=session_name,
         )
