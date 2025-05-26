d7155743a31c0c62b22c5276d3b8a0e81ed2153b
Alessio Buccino <alejoe9187@gmail.com>
2025-05-26 16:33:25 +0200

    update --params 2

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -71,11 +71,15 @@ if __name__ == "__main__":
     PARAMS = args.params
 
     if PARAMS is not None:
-        if Path(PARAMS).is_file():
-            with open(PARAMS, "r") as f:
-                visualization_params = json.load(f)
-        else:
+        try:
+            # try to parse the JSON string first to avoid file name too long error
             visualization_params = json.loads(PARAMS)
+        except json.JSONDecodeError:
+            if Path(PARAMS).is_file():
+                with open(PARAMS, "r") as f:
+                    visualization_params = json.load(f)
+            else:
+                raise ValueError(f"Invalid parameters: {PARAMS} is not a valid JSON string or file path")
     else:
         with open("params.json", "r") as f:
             visualization_params = json.load(f)
